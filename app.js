632fbe8949862af764f4881c8d37ee36cd63d7ed
Tuesday-Last <sgnortz@gmail>
2016-01-20 19:37:15 -0800

    Got the first stor location working

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
+var pikeList = document.getElementById('pike');
 var pikePlace = {
   customerMin: 17,
-  cuntomerMax: 88,
-  avg: 5.2
-  bake: []
-}
+  customerMax: 88,
+  avg: 5.2,
+  bake: [],
+  }
 
 toBake = function(store) {
   var randomCust = Math.floor(Math.random() * (store.customerMax - store.customerMin + 1)) + store.customerMin;
-  store.bake.push(randomCust * store.avg);
+  store.bake.push(Math.floor((randomCust * store.avg)));
+  console.log("customers = " + randomCust);
   }
 
-for (hour in hours) {
-  var liCookies = document.createElement('li');
-  liCookies.textContent = hours[hour] + bake[cookies];
+for (var i =0; i < hours.length; i++) {
+  toBake(pikePlace);
 } 
 
-.appendChild(liCookies);
+for (var i =0; i < hours.length; i++) {
+  var liCookies = document.createElement('li');
+  liCookies.textContent = hours[i] + pikePlace.bake[i];
+  pikeList.appendChild(liCookies);
+} 
+
+var pikePlaceTotal = 0
+for (var i = 0; i < pikePlace.bake.length; i++){
+  pikePlaceTotal = pikePlaceTotal + pikePlace.bake[i];
+  }
+
+var liTotal = document.createElement('li');
+  liTotal.textContent = "Total: " + pikePlaceTotal;
+  pikeList.appendChild(liTotal);
